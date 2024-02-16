990706d64596ff8a5176094884f04d343fa5cf53
Pascal Baljet <pascal@protone.media>
2024-02-16 16:24:30 +0100

    Update BuildComponents.php

--- src/Commands/BuildComponents.php
+++ src/Commands/BuildComponents.php
@@ -26,46 +26,42 @@ class BuildComponents extends Command
             config(['splade-core.prettify_compiled_scripts' => false]);
         }
 
-        foreach (['', '/components/splade'] as $sub) {
-            foreach (config('view.paths') as $path) {
-                $path = $path.$sub;
+        foreach (config('view.paths') as $path) {
+            if (! $filesystem->exists($path)) {
+                continue;
+            }
 
-                if (! $filesystem->exists($path)) {
+            $this->info("Searching in {$path}");
+
+            $files = $filesystem->allFiles($path);
+
+            // sort by length so that the longest path is first
+            usort($files, fn ($a, $b) => strlen($a) <=> strlen($b));
+
+            foreach ($files as $view) {
+                /** @var SplFileInfo $view */
+                $viewPath = (string) $view;
+
+                if (! str_ends_with($viewPath, '.blade.php')) {
                     continue;
                 }
 
-                $this->info("Searching in {$path}");
+                $contents = $filesystem->get($viewPath);
 
-                $files = $filesystem->allFiles($path);
-
-                // sort by length so that the longest path is first
-                usort($files, fn ($a, $b) => strlen($a) <=> strlen($b));
-
-                foreach ($files as $view) {
-                    /** @var SplFileInfo $view */
-                    $viewPath = (string) $view;
-
-                    if (! str_ends_with($viewPath, '.blade.php')) {
-                        continue;
-                    }
-
-                    $contents = $filesystem->get($viewPath);
-
-                    if (! str_contains($contents, '<script setup>')) {
-                        continue;
-                    }
-
-                    $this->info("Compiling {$viewPath}");
-
-                    $componentClass = $componentHelper->getClass($viewPath);
-
-                    BladeViewExtractor::from($contents, ['spladeBridge' => [
-                        'data' => $componentClass ? ComponentSerializer::getDataFromComponentClass($componentClass) : [],
-                        'props' => $componentClass ? ComponentSerializer::getPropsFromComponentClass($componentClass) : [],
-                        'tag' => $componentHelper->getTag($viewPath),
-                        'functions' => $componentClass ? ComponentSerializer::getFunctionsFromComponentClass($componentClass) : [],
-                    ]], $viewPath)->handle($filesystem);
+                if (! str_contains($contents, '<script setup>')) {
+                    continue;
                 }
+
+                $this->info("Compiling {$viewPath}");
+
+                $componentClass = $componentHelper->getClass($viewPath);
+
+                BladeViewExtractor::from($contents, ['spladeBridge' => [
+                    'data' => $componentClass ? ComponentSerializer::getDataFromComponentClass($componentClass) : [],
+                    'props' => $componentClass ? ComponentSerializer::getPropsFromComponentClass($componentClass) : [],
+                    'tag' => $componentHelper->getTag($viewPath),
+                    'functions' => $componentClass ? ComponentSerializer::getFunctionsFromComponentClass($componentClass) : [],
+                ]], $viewPath)->handle($filesystem);
             }
         }
 
